647cf4a35d5532f34ad3bf80ea4d602f6658a6d3
AbhilashNelson <abhilash.nelson@faithinfotech.in>
2025-09-16 09:45:18 +0530

    changed backend-ems-fastapi to backend

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/backend-ems-fastapi
+cd /home/ec2-user/backend
 source venv/bin/activate
 nohup gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:8000 > gunicorn.log 2>&1 &
